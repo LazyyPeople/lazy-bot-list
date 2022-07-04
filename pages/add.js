41db08634a504cb6daec5e65a055330a9b0576dd
jeremy776 <jeremy.kusuma2018@gmail.com>
2022-07-05 00:54:20 +0700

    uh

--- pages/add.js
+++ pages/add.js
@@ -6,12 +6,21 @@ import {
     Container,
     Flex,
     Text,
-    ListItem,
+    Stack,
+    Radio,
+    RadioGroup,
     Button,
     Textarea,
-    List,
+    Alert,
+    AlertIcon,
+    AlertDescription,
     useDisclosure,
     Modal,
+    Accordion,
+    AccordionIcon,
+    AccordionItem,
+    AccordionPanel,
+    AccordionButton,
     ModalContent,
     ModalOverlay,
     ModalCloseButton,
@@ -27,11 +36,13 @@ import Footer from '../components/footer/Footer';
 import {
     mdToHtml
 } from '../utils/markdown';
+import config from "../utils/config";
 import { useRef, useState } from "react";
 import { Prose } from "@nikolovlazar/chakra-ui-prose";
 import DOMPurify from "dompurify";
 
 export default function AddBot({ user }) {
+    const [valueRadio, setValueRadio] = useState(0);
 
     const {
         isOpen,
@@ -47,6 +58,65 @@ export default function AddBot({ user }) {
         setHtml(p);
         return;
     }
+
+    const [error, setError] = useState(null);
+
+    async function _handleSubmit() {
+        let botID = document.getElementById('idbot');
+
+        // validate bot
+        let app = await fetch('https://discord.com/api/v9/applications/' + botID.value + '/rpc', {
+            headers: {
+                'Authorization': `Bearer ${config.discord.api.authorization}`
+            }
+        }).then(x => x.json());
+
+        let responCode = {
+            "10002": {
+                errorType: 'Unknown App',
+                message: 'Bot could not found'
+            },
+            "50035": {
+                errorType: 'Invalid ID',
+                message: 'invalid id provided'
+            },
+            "0": {
+                errorType: 'Unauthorized',
+                message: '401: Unauthorized'
+            },
+
+            // Custom code
+            "-10001001": {
+                errorType: "Forbidden (not public)",
+                message: 'Your bot is not public'
+            },
+            "-10001002": {
+                errorType: 'Forbidden (bot require code grant)',
+                message: 'Bot can\'t add if require code grant'
+            }
+        }
+        if(app.code !== undefined) {
+            setError(responCode[app.code]);
+            window.scrollTo(0,0);
+            return;
+        }
+        if (!app.bot_public) {
+            setError(responCode["-10001001"]);
+            window.scrollTo(0,0);
+            return;
+        }
+        if(app.bot_require_code_grant) {
+            setError(responCode["-10001002"]);
+            window.scrollTo(0,0);
+            return;
+        }
+
+
+
+        // Prefix
+
+    }
+
     return (
         <>
             <Head title={'{name} - Add Bot'} />
@@ -55,6 +125,10 @@ export default function AddBot({ user }) {
             <Container maxW={'3xl'} mt={'6'} mb={20}>
                 <Flex flexDirection={{ base: 'column', md: 'column' }} gap={10}>
                     <Box>
+                        {error && <Alert status='error'>
+                            <AlertIcon />
+                            <AlertDescription>{error.message}</AlertDescription>
+                        </Alert>}
                         <Flex mt={'7'} flexDirection={'column'} gap={'4'}>
                             <FormControl isRequired>
                                 <FormLabel fontWeight={'medium'} color={'gray.600'} htmlFor='idbot'>Bot ID</FormLabel>
@@ -63,6 +137,29 @@ export default function AddBot({ user }) {
                             <FormControl isRequired>
                                 <FormLabel fontWeight={'medium'} color={'gray.600'} htmlFor="prefix">Prefix</FormLabel>
                                 <Input id='prefix' fontSize={'sm'} placeholder="!" />
+                                <Accordion allowToggle mt={2}>
+                                    <AccordionItem borderWidth={0}>
+                                        <Text>
+                                            <AccordionButton>
+                                                <Box fontSize={'sm'} color={'gray.500'} flex='1' textAlign={'left'}>
+                                                    My bot uses slash command
+                                                </Box>
+                                                <AccordionIcon />
+                                            </AccordionButton>
+                                        </Text>
+                                        <AccordionPanel>
+                                            <RadioGroup onChange={setValueRadio} value={valueRadio}>
+                                                <Stack color={'gray.500'}>
+                                                    <Radio size={'md'} value='s'>Slash Command</Radio>
+                                                    <Radio size={'md'} value='sp'>Slash Command and Prefix</Radio>
+                                                    <Box pt={4}>
+                                                        <Text color={'telegram.500'} onClick={() => setValueRadio(0)} cursor={'pointer'}>Clear Options</Text>
+                                                    </Box>
+                                                </Stack>
+                                            </RadioGroup>
+                                        </AccordionPanel>
+                                    </AccordionItem>
+                                </Accordion>
                             </FormControl>
                             <FormControl>
                                 <FormLabel fontWeight={'medium'} color={'gray.600'} htmlFor="owners">Owners</FormLabel>
@@ -94,7 +191,7 @@ export default function AddBot({ user }) {
                             </FormControl>
 
                             <FormControl>
-                                <Button colorScheme={'messenger'} px={8} size={'md'} mt={3}>Submit Bot</Button>
+                                <Button onClick={() => _handleSubmit()} colorScheme={'messenger'} px={8} size={'md'} mt={3}>Submit Bot</Button>
                             </FormControl>
                         </Flex>
                     </Box>
@@ -121,7 +218,7 @@ export default function AddBot({ user }) {
                         </Prose>
                     </ModalBody>
                     <ModalFooter>
-                        <Text fontSize={{base: 'xs', md: 'sm'}} color={'gray.600'} fontWeight={'medium'}>*This is just a preview, maybe the text size will change when on the page view bot</Text>
+                        <Text fontSize={{ base: 'xs', md: 'sm' }} color={'gray.600'} fontWeight={'medium'}>*This is just a preview, maybe the text size will change when on the page view bot</Text>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
